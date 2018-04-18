0573931fe5b7d85087117043b712ef41c0b8c8fb
xuankuai91 <xuankuai91@gmail.com>
2018-04-18 09:23:31 -0500

    Add plots

--- monte_carlo_simulation/Monte_Carlo_Simulation_Regression.py
+++ monte_carlo_simulation/Monte_Carlo_Simulation_Regression.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 reload(sys)
@@ -78,17 +79,18 @@ df['TransitRate'] = 100 * df.apply(lambda x: SimulateRatio(x['Rij'], x['B08301e1
 
 # Simulate the independent values:
 # Female-male ratio
-df['FMRatio'] = df.apply(lambda x: SimulateRatio(x['Rij'], x['B01001e2'], x['B01001m2'], x['B01001e26'], x['B01001m26']), axis=1)
-df.loc[df['FMRatio'] > 10, 'FMRatio'] = 1 # Set outliers to normal values
+df['FHHRate'] = 100 * df.apply(lambda x: SimulateRatio(x['Rij'], x['B11001e6'], x['B11001m6'], x['B11001e1'], x['B11001m1']), axis=1)
+df.loc[df['FHHRate'] > 100, 'FHHRate'] = 100 # Set outliers to normal values
 
-# College diploma percentage
-df['Bachelor'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B15003e22'], x['B15003m22']), axis=1)
-df['Master'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B15003e23'], x['B15003m23']), axis=1)
-df['Professional'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B15003e24'], x['B15003m24']), axis=1)
-df['Doctor'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B15003e25'], x['B15003m25']), axis=1)
-df['Subtotal'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B15003e1'], x['B15003m1']), axis=1)
-df['CollegeRate'] = 100 * (df['Bachelor'] + df['Master'] + df['Professional'] + df['Doctor']) / df['Subtotal']
-df.loc[df['CollegeRate'] > 100, 'CollegeRate'] = 100 # Set outliers to normal values
+# Renter-occupied household percentage
+df['RenterRate'] = 100 * df.apply(lambda x: SimulateRatio(x['Rij'], x['B25003e3'], x['B25003m3'], x['B25003e1'], x['B25003m1']), axis=1)
+df.loc[df['RenterRate'] > 100, 'RenterRate'] = 100 # Set outliers to normal values
+
+# Carless household percentage
+df['Carless'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e3'], x['B25044m3']), axis=1) + df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e10'], x['B25044m10']), axis=1)
+df['Subtotal'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e1'], x['B25044m1']), axis=1)
+df['CarlessRate'] = 100 * (df['Carless'] / df['Subtotal'])
+df.loc[df['CarlessRate'] > 100, 'CarlessRate'] = 100 # Set outliers to normal values
 
 # Poverty rate
 df['Poverty'] = df.apply(lambda x: SimulateValue(x['Rij'], x['C17002e2'], x['C17002m2']), axis=1) + df.apply(lambda x: SimulateValue(x['Rij'], x['C17002e3'], x['C17002m3']), axis=1)
@@ -99,44 +101,30 @@ df.loc[df['PovertyRate'] > 100, 'PovertyRate'] = 100 # Set outliers to normal va
 # Median household income
 df['Income'] = df.apply(lambda x: SimulateValue(1, x['B19013e1'], x['B19013m1']), axis=1)
 
-# Renter-occupied household percentage
-df['Renter'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25003e3'], x['B25003m3']), axis=1)
-df['Subtotal'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25003e1'], x['B25003m1']), axis=1)
-df['RenterRate'] = 100 * (df['Renter'] / df['Subtotal'])
-df.loc[df['RenterRate'] > 100, 'RenterRate'] = 100 # Set outliers to normal values
-
-# Carless household percentage
-df['Carless'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e3'], x['B25044m3']), axis=1) + df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e10'], x['B25044m10']), axis=1)
-df['Subtotal'] = df.apply(lambda x: SimulateValue(x['Rij'], x['B25044e1'], x['B25044m1']), axis=1)
-df['CarlessRate'] = 100 * (df['Carless'] / df['Subtotal'])
-df.loc[df['CarlessRate'] > 100, 'CarlessRate'] = 100 # Set outliers to normal values
-
 # Select fields to keep
-df = df[['EBRP_Block_Pg_BLOCKID10', 'BLKGRPID10', 'Rij', 'TransitRate', 'FMRatio', 'CollegeRate', 'PovertyRate', 'Income', 'RenterRate', 'CarlessRate', 'MedAge', 'PctWhite', 'LDR', 'Ride2Drive']]
+df = df[['BLOCKID10', 'BLKGRPID10', 'Rij', 'TransitRate', 'FHHRate', 'PctWhite', 'RenterRate', 'CarlessRate', 'PovertyRate', 'Income', 'LDR', 'Ride2Drive', 'NEAR_DIST', 'Emp_60min']]
 
 # Export to a CSV file
 try:
 	df.to_csv("TransitRate.csv")
 except:
-	pass
+	os.remove("TransitRate.csv") # Delete existing file
+	df.to_csv("TransitRate.csv")
 
 # Draw scatter and regression line plots
-fig = plt.figure(figsize=(8.5, 11))
+# Non-spatial variables
+fig1 = plt.figure(figsize=(8.5, 6.6))
 seq = 0
-dict = {'FMRatio': 'Female-male ratio',
-		'CollegeRate': 'College diploma percentage',
-		'PovertyRate': 'Poverty rate',
-		'Income': 'Median household income',
-		'RenterRate': 'Renter-occupied housing percentage',
-		'CarlessRate': 'Carless household percentage',
-		'MedAge': 'Median age',
-		'PctWhite': 'White percentage',
-		'LDR': 'Low density residential',
-		'Ride2Drive': 'Bus-riding-driving time ratio'}
+non_spatial_dict = {'FHHRate': 'Female household percentage',
+					'PctWhite': 'White population percentage',
+					'RenterRate': 'Renter-occupied housing percentage',
+					'CarlessRate': 'Carless household percentage',
+					'PovertyRate': 'Poverty rate',
+					'Income': 'Median household income'}
 
-for item in ['FMRatio', 'CollegeRate', 'PovertyRate', 'Income', 'RenterRate', 'CarlessRate', 'MedAge', 'PctWhite', 'LDR', 'Ride2Drive']:
+for item in ['FHHRate', 'PctWhite', 'RenterRate', 'CarlessRate', 'PovertyRate', 'Income']:
 	seq += 1
-	ax = fig.add_subplot(5, 2, seq)
+	ax = fig1.add_subplot(3, 2, seq)
 
 	# Scatter
 	ax.scatter(df[item], df['TransitRate'], s=2)
@@ -151,10 +139,41 @@ for item in ['FMRatio', 'CollegeRate', 'PovertyRate', 'Income', 'RenterRate', 'C
 	ax.plot(x, y, color='C1')
 
 	# Configure layout
-	plt.title(dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
+	plt.title(non_spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
 	plt.ylim((0, 70)) # Set X-axis range
 	plt.tight_layout() # Optimize layout
 	AdjustSpines(ax, ['left', 'bottom'])
 
 plt.savefig('Figure 10.jpg', dpi=300)
-plt.show()
+
+# Spatial variables
+fig2 = plt.figure(figsize=(8.5, 4.4))
+seq = 0
+spatial_dict = {'LDR': 'Low density residential',
+				'Ride2Drive': 'Transit-driving time ratio',
+				'NEAR_DIST': 'Distance to nearest stop',
+				'Emp_60min': 'Number of jobs in 30 minutes'}
+
+for item in ['LDR', 'Ride2Drive', 'NEAR_DIST', 'Emp_60min']:
+	seq += 1
+	ax = fig2.add_subplot(2, 2, seq)
+
+	# Scatter
+	ax.scatter(df[item], df['TransitRate'], s=2)
+
+	# Regression line
+	result = sm.ols(formula=("TransitRate ~ " + item), data=df).fit()
+	k = result.params[1]
+	b = result.params[0]
+
+	x = np.linspace(0, df.loc[df[item].idxmax(), item])
+	y = k * x + b
+	ax.plot(x, y, color='C1')
+
+	# Configure layout
+	plt.title(spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
+	plt.ylim((0, 70)) # Set X-axis range
+	plt.tight_layout() # Optimize layout
+	AdjustSpines(ax, ['left', 'bottom'])
+
+plt.savefig('Figure 11.jpg', dpi=300)
