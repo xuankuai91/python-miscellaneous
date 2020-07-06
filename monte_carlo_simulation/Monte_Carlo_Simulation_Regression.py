bfa930ebfd1b877ab61dcbf5195e2f56f75d9406
Xuan Kuai <xuankuai91@gmail.com>
2020-07-06 14:33:38 -0500

    Convert tab to 4 spaces

--- monte_carlo_simulation/Monte_Carlo_Simulation_Regression.py
+++ monte_carlo_simulation/Monte_Carlo_Simulation_Regression.py
@@ -13,43 +13,43 @@ from scipy.stats import truncnorm
 
 # Simulate single value
 def SimulateValue(Rij, Est, MoE):
-	# Set initial values
-	epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
-	estimate = -1
+    # Set initial values
+    epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
+    estimate = -1
 
-	# Check if negative values are simulated
-	while estimate < 0:
-		# Re-simulate
-		epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
-		estimate = Rij * (Est + MoE * epsilon)
+    # Check if negative values are simulated
+    while estimate < 0:
+        # Re-simulate
+        epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
+        estimate = Rij * (Est + MoE * epsilon)
 
-		# Stop simulating if values are positive
-		if estimate >= 0:
-			break
+        # Stop simulating if values are positive
+        if estimate >= 0:
+            break
 
-	return estimate
+    return estimate
 
 # Simulate ratio
 def SimulateRatio(Rij, NumerEst, NumerMoE, DenomEst, DenomMoE):
-	# Set initial values
-	epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
-	numerator = -1
-	denominator = -1
+    # Set initial values
+    epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
+    numerator = -1
+    denominator = -1
 
-	# Check if negative values are simulated
-	while numerator < 0 or denominator <= 0:
-		# Re-simulate
-		epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
-		numerator = NumerEst + NumerMoE * epsilon
-		denominator = DenomEst + DenomMoE * epsilon
+    # Check if negative values are simulated
+    while numerator < 0 or denominator <= 0:
+        # Re-simulate
+        epsilon = truncnorm(-1.645, 1.645, loc=0, scale=1).rvs()
+        numerator = NumerEst + NumerMoE * epsilon
+        denominator = DenomEst + DenomMoE * epsilon
 
-		# Stop simulating if values are positive
-		if numerator >= 0 and denominator > 0:
-			break
+        # Stop simulating if values are positive
+        if numerator >= 0 and denominator > 0:
+            break
 
-	estimate = numerator / denominator
+    estimate = numerator / denominator
 
-	return estimate
+    return estimate
 
 # Modify axes of plot
 def AdjustSpines(ax, spines):
@@ -106,43 +106,43 @@ df = df[['BLOCKID10', 'BLKGRPID10', 'Rij', 'TransitRate', 'FHHRate', 'PctWhite',
 
 # Export to a CSV file
 try:
-	df.to_csv("TransitRate.csv")
+    df.to_csv("TransitRate.csv")
 except:
-	os.remove("TransitRate.csv") # Delete existing file
-	df.to_csv("TransitRate.csv")
+    os.remove("TransitRate.csv") # Delete existing file
+    df.to_csv("TransitRate.csv")
 
 # Draw scatter and regression line plots
 # Non-spatial variables
 fig1 = plt.figure(figsize=(8.5, 6.6))
 seq = 0
 non_spatial_dict = {'FHHRate': 'Female household percentage',
-					'PctWhite': 'White population percentage',
-					'RenterRate': 'Renter-occupied housing percentage',
-					'CarlessRate': 'Carless household percentage',
-					'PovertyRate': 'Poverty rate',
-					'Income': 'Median household income'}
+                    'PctWhite': 'White population percentage',
+                    'RenterRate': 'Renter-occupied housing percentage',
+                    'CarlessRate': 'Carless household percentage',
+                    'PovertyRate': 'Poverty rate',
+                    'Income': 'Median household income'}
 
 for item in ['FHHRate', 'PctWhite', 'RenterRate', 'CarlessRate', 'PovertyRate', 'Income']:
-	seq += 1
-	ax = fig1.add_subplot(3, 2, seq)
+    seq += 1
+    ax = fig1.add_subplot(3, 2, seq)
 
-	# Scatter
-	ax.scatter(df[item], df['TransitRate'], s=2)
+    # Scatter
+    ax.scatter(df[item], df['TransitRate'], s=2)
 
-	# Regression line
-	result = sm.ols(formula=("TransitRate ~ " + item), data=df).fit()
-	k = result.params[1]
-	b = result.params[0]
+    # Regression line
+    result = sm.ols(formula=("TransitRate ~ " + item), data=df).fit()
+    k = result.params[1]
+    b = result.params[0]
 
-	x = np.linspace(0, df.loc[df[item].idxmax(), item])
-	y = k * x + b
-	ax.plot(x, y, color='C1')
+    x = np.linspace(0, df.loc[df[item].idxmax(), item])
+    y = k * x + b
+    ax.plot(x, y, color='C1')
 
-	# Configure layout
-	plt.title(non_spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
-	plt.ylim((0, 70)) # Set X-axis range
-	plt.tight_layout() # Optimize layout
-	AdjustSpines(ax, ['left', 'bottom'])
+    # Configure layout
+    plt.title(non_spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
+    plt.ylim((0, 70)) # Set X-axis range
+    plt.tight_layout() # Optimize layout
+    AdjustSpines(ax, ['left', 'bottom'])
 
 plt.savefig('Figure 10.jpg', dpi=300)
 
@@ -150,30 +150,30 @@ plt.savefig('Figure 10.jpg', dpi=300)
 fig2 = plt.figure(figsize=(8.5, 4.4))
 seq = 0
 spatial_dict = {'LDR': 'Low density residential',
-				'Ride2Drive': 'Transit-driving time ratio',
-				'NEAR_DIST': 'Distance to nearest stop',
-				'Emp_60min': 'Number of jobs in 30 minutes'}
+                'Ride2Drive': 'Transit-driving time ratio',
+                'NEAR_DIST': 'Distance to nearest stop',
+                'Emp_60min': 'Number of jobs in 30 minutes'}
 
 for item in ['LDR', 'Ride2Drive', 'NEAR_DIST', 'Emp_60min']:
-	seq += 1
-	ax = fig2.add_subplot(2, 2, seq)
+    seq += 1
+    ax = fig2.add_subplot(2, 2, seq)
 
-	# Scatter
-	ax.scatter(df[item], df['TransitRate'], s=2)
+    # Scatter
+    ax.scatter(df[item], df['TransitRate'], s=2)
 
-	# Regression line
-	result = sm.ols(formula=("TransitRate ~ " + item), data=df).fit()
-	k = result.params[1]
-	b = result.params[0]
+    # Regression line
+    result = sm.ols(formula=("TransitRate ~ " + item), data=df).fit()
+    k = result.params[1]
+    b = result.params[0]
 
-	x = np.linspace(0, df.loc[df[item].idxmax(), item])
-	y = k * x + b
-	ax.plot(x, y, color='C1')
+    x = np.linspace(0, df.loc[df[item].idxmax(), item])
+    y = k * x + b
+    ax.plot(x, y, color='C1')
 
-	# Configure layout
-	plt.title(spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
-	plt.ylim((0, 70)) # Set X-axis range
-	plt.tight_layout() # Optimize layout
-	AdjustSpines(ax, ['left', 'bottom'])
+    # Configure layout
+    plt.title(spatial_dict.get(item) + ' (' + str("{0:.3f}".format(result.pvalues[1])) + ')') # Write subplot's title with p-value
+    plt.ylim((0, 70)) # Set X-axis range
+    plt.tight_layout() # Optimize layout
+    AdjustSpines(ax, ['left', 'bottom'])
 
 plt.savefig('Figure 11.jpg', dpi=300)
