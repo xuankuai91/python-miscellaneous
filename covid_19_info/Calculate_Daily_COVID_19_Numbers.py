324b356c0f361f3941577754b8d488ed08276669
Xuan Kuai <xuankuai91@gmail.com>
2020-10-22 09:53:29 -0500

    Use functions in custom module

--- covid_19_info/Calculate_Daily_COVID_19_Numbers.py
+++ covid_19_info/Calculate_Daily_COVID_19_Numbers.py
@@ -1,192 +1,122 @@
-import arcpy, datetime
-
-def copy_case_counts(table, fields, county_summary, day):
-    try:
-        # Insert a new row to table
-        target_cursor = arcpy.InsertCursor(table)
-        target_row = target_cursor.newRow()
-
-        # Write date of the day before the execution date
-        date = "{}/{}/{}".format(day.year, day.month, day.day)
-        target_row.setValue("Date_", date)
-        target_cursor.insertRow(target_row)
-
-    except:
-        continue
-
-    # Compose date query of the day before the execution date (for the latest cases data)
-    this_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(day.year, day.month, day.day)
-    this_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + this_day_date
-
-    # Copy case count from HGAC_Counties_COVID_19_Cases to table
-    arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
-
-    county_cursor = arcpy.da.SearchCursor(county_summary, fields)
-
-    for county_row in county_cursor:
-        county_name = county_row[0]
-        value = county_row[1]
-
-        if county_name == "Fort Bend":
-            county_name = "Fort_Bend"
-
-        print("        " + county_name)
-        arcpy.CalculateField_management('table_tv', county_name, str(value))
-    
+import arcpy, datetime, sys
+sys.path.append(r'\\Hgac.net\FileShare\ArcGIS\DataSvcs\GIS\Process Automation\Modules\Bin')
+import earcpy
 
 def calculate_change(table, day):
     # Calculate total number of cases
-    print("        Total")
-    arcpy.CalculateField_management(table, "Total", "[Austin] + [Brazoria] + [Chambers] + [Colorado] + [Fort_Bend] + [Galveston] + [Harris] + [Liberty] + [Matagorda] + [Montgomery] + [Walker] + [Waller] + [Wharton]")
-
-    print("        Change")
-
-    # Compose date query of the day before the execution date (for the latest cases data)
-    this_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(day.year, day.month, day.day)
-    this_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + this_day_date
-
-    # Compose date query of two days before the execution date (for the previous cases data)
-    previous_day = day - datetime.timedelta(1)
-    previous_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_day.year, previous_day.month, previous_day.day)
-    previous_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_day_date
-
-    # Calculate change in cases
-    this_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Total"], this_day_query)
-    for this_day_row in this_day_cursor:
-        this_day_total = this_day_row[1]
-
-    previous_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Total"], previous_day_query)
-    for previous_day_row in previous_day_cursor:
-        previous_day_total = previous_day_row[1]
-
-    change = this_day_total - previous_day_total
+    print("                Total")
+    # Compose date query of the day intended to revise
+    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
 
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
 
+    arcpy.CalculateField_management(table, "Total", "[Austin] + [Brazoria] + [Chambers] + [Colorado] + [Fort_Bend] + [Galveston] + [Harris] + [Liberty] + [Matagorda] + [Montgomery] + [Walker] + [Waller] + [Wharton]")
+
+    print("                Change")
+    # Compose date query of the previous day before the day intended to revise
+    previous_day = day - datetime.timedelta(1)
+    previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
+
+    # Calculate change in case
+    change = earcpy.calculate_difference(table, "Total", this_day_query, previous_day_query)
+
     arcpy.CalculateField_management('table_tv', "Change", str(change))
-    
-    print("        Mov_Avg")
+
+    print("                Mov_Avg")
 
     # Compose date query of the 7-day period from a week before the execution date to the previous day of the execution date
-    past_week_day = day - datetime.timedelta(6)
-    past_week_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(past_week_day.year, past_week_day.month, past_week_day.day)
-    past_week_query = arcpy.AddFieldDelimiters(table, "Date_") + " BETWEEN " + past_week_day_date + " AND " + this_day_date
+    past_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=7)
 
     # Calculate 7-day average change in cases
-    past_week_cursor = arcpy.da.SearchCursor(table, ["Date_", "Change"], past_week_query)
-    change_list = [past_week_row[1] for past_week_row in past_week_cursor if past_week_row[1] is not None]
+    change_list = earcpy.list_values(table, "Change", past_week_query)
     moving_average = float(sum(change_list)) / len(change_list)
 
     arcpy.CalculateField_management('table_tv', "Mov_Avg", str(moving_average))
 
-def calculate_test_count(table, county_summary, day):
-    print("        Tests")
-    county_cursor = arcpy.da.SearchCursor(county_summary, ["NAME", "No_of_Tests"])
-
-    test_list = [county_row[1] for county_row in county_cursor]
-    total_tests = sum(test_list)
-
-    # Compose date query of two days before the execution date (for the previous cases data)
-    previous_day = day - datetime.timedelta(1)
-    previous_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_day.year, previous_day.month, previous_day.day)
-    previous_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_day_date
-
-    arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
-
-    arcpy.CalculateField_management('table_tv', "Tests", str(total_tests))
-
-def calculate_positivities(table, county_summary, day):
-    print("        Positivity")
+def calculate_positivities(table, day):
+    print("            Positivity")
     # Compose date query of the day before the execution date (for the latest cases data)
-    previous_day = day - datetime.timedelta(1)
-    previous_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_day.year, previous_day.month, previous_day.day)
-    previous_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_day_date
+    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
 
     arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
+    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
     arcpy.CalculateField_management('table_tv', "Positivity", "100 * [Total] / [Tests]")
 
-    previous_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Tests"], previous_day_query)
-    for previous_day_row in previous_day_cursor:
-        previous_day_tests = previous_day_row[1]
-
-    print("        Positivity_Mov_Avg")
-    # Compose date query of two days before the execution date (for the previous tests data)
-    previous_2_day = day - datetime.timedelta(2)
-    previous_2_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_2_day.year, previous_2_day.month, previous_2_day.day)
-    previous_2_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_2_day_date
-
-    previous_2_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Tests"], previous_2_day_query)
-    for previous_2_day_row in previous_2_day_cursor:
-        previous_2_day_tests = previous_2_day_row[1]
-
-    arcpy.CalculateField_management('table_tv', "Tests_Change", str(previous_day_tests - previous_2_day_tests))
-
+    print("            Positivity_Mov_Avg")
     # Compose date query of the 7-day period from 1 week before the previous day of the execution date to two days before the execution date
-    previous_week_day = day - datetime.timedelta(7)
-    previous_week_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_week_day.year, previous_week_day.month, previous_week_day.day)
-    previous_week_query = arcpy.AddFieldDelimiters(table, "Date_") + " BETWEEN " + previous_week_day_date + " AND " + previous_day_date
+    past_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=7)
 
-    # Calculate 7-day average positivity rate
-    previous_week_cursor = arcpy.da.SearchCursor(table, ["Date_", "Change", "Tests_Change"], previous_week_query)
-
-    confirmed_change_list = []
-    tests_change_list = []
-    for previous_week_row in previous_week_cursor:
-        if previous_week_row[1] is not None:
-            confirmed_change_list.append(previous_week_row[1])
-
-        if previous_week_row[2] is not None:
-            tests_change_list.append(previous_week_row[2])
+    confirmed_change_list = earcpy.list_values(table, "Change", past_week_query)
+    tests_change_list = earcpy.list_values(table, "Tests_Change", past_week_query)
 
     positivity_moving_average = 100 * float(sum(confirmed_change_list)) / sum(tests_change_list)
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
     arcpy.CalculateField_management('table_tv', "Positivity_Mov_Avg", str(positivity_moving_average))
-
-def calculate_table(table, fields, county_summary, day):
-    copy_case_counts(table, fields, county_summary, day)
-    calculate_change(table, day)
 
 def main():
     arcpy.env.workspace = r"Database Connections\Global_SDE_(Global_Admin).sde"
     arcpy.env.overwriteOutput = True
 
-    yesterday = datetime.datetime.today() - datetime.timedelta(1)
-
-    county_summary = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Info\Global.GLOBAL_ADMIN.HGAC_Counties_COVID_19_Cases"
-    arcpy.CalculateField_management(county_summary, "No_of_Cases", "[No_of_Actives] + [No_of_Deaths] + [No_of_Recoveries]") # Calculate total number of cases
-
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Confirmed_Cases_and_Tests, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Confirmed_Cases_and_Tests")
+    print("    Revising COVID-19 case data ...")
+    this_day_date = raw_input("Please enter the day intended to revise (m/d/yyyy): ")
+    this_day = datetime.datetime.strptime(this_day_date, "%m/%d/%Y")
+    order_clause = (None, "ORDER BY Date_")
+    
+    # Calculate changes in confirmed cases for the day intended to revise and the day before
+    print("        HGAC_COVID_19_Confirmed_Cases_and_Tests")
     confirmed_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Confirmed_Cases_and_Tests"
-    calculate_table(confirmed_table, ["NAME", "No_of_Cases"], county_summary, yesterday)
-    calculate_test_count(confirmed_table, county_summary, yesterday)
-    calculate_positivities(confirmed_table, county_summary, yesterday)
 
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Active_Cases, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Active_Cases")
+    future_days_query = earcpy.compose_single_date_query(confirmed_table, "Date_", this_day, ">=")
+    future_days_cursor = arcpy.da.SearchCursor(confirmed_table, ["Date_", "Total", "Change", "Mov_Avg"], future_days_query, sql_clause=order_clause)
+    for future_days_row in future_days_cursor:
+        current_day = future_days_row[0]
+        print("            " + str(current_day))
+
+        calculate_change(confirmed_table, current_day)
+        calculate_positivities(confirmed_table, current_day)
+
+    # Calculate changes in active cases for the day intended to revise and the day before
+    print("        HGAC_COVID_19_Active_Cases")
     active_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Active_Cases"
-    calculate_table(active_table, ["NAME", "No_of_Actives"], county_summary, yesterday)
+    
+    future_days_query = earcpy.compose_single_date_query(active_table, "Date_", this_day, ">=")
+    future_days_cursor = arcpy.da.SearchCursor(active_table, ["Date_", "Total", "Change", "Mov_Avg"], future_days_query, sql_clause=order_clause)
+    for future_days_row in future_days_cursor:
+        current_day = future_days_row[0]
+        print("            " + str(current_day))
 
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Deceased_Cases, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Deceased_Cases")
+        calculate_change(active_table, current_day)
+
+    # Calculate changes in deceased cases for the day intended to revise and the day before
+    print("        HGAC_COVID_19_Deceased_Cases")
     deceased_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Deceased_Cases"
-    calculate_table(deceased_table, ["NAME", "No_of_Deaths"], county_summary, yesterday)
+    
+    future_days_query = earcpy.compose_single_date_query(deceased_table, "Date_", this_day, ">=")
+    future_days_cursor = arcpy.da.SearchCursor(deceased_table, ["Date_", "Total", "Change", "Mov_Avg"], future_days_query, sql_clause=order_clause)
+    for future_days_row in future_days_cursor:
+        current_day = future_days_row[0]
+        print("            " + str(current_day))
 
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Recovered_Cases, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Recovered_Cases")
+        calculate_change(deceased_table, current_day)
+
+    # Calculate changes in recovered cases for the day intended to revise and the day before
+    print("        HGAC_COVID_19_Recovered_Cases")
     recovered_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Recovered_Cases"
-    calculate_table(recovered_table, ["NAME", "No_of_Recoveries"], county_summary, yesterday)
+    
+    future_days_query = earcpy.compose_single_date_query(recovered_table, "Date_", this_day, ">=")
+    future_days_cursor = arcpy.da.SearchCursor(recovered_table, ["Date_", "Total", "Change", "Mov_Avg"], future_days_query, sql_clause=order_clause)
+    for future_days_row in future_days_cursor:
+        current_day = future_days_row[0]
+        print("            " + str(current_day))
+
+        calculate_change(recovered_table, current_day)
 
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
-    print("Starting Calculate Daily COVID-19 Numbers tool")
-    print("Version 1.2")
-    print("Last update: 9/24/2020")
+    print("Starting Revise COVID-19 Numbers tool")
+    print("Version 1.1")
+    print("Last update: 10/20/2020")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
     print("Start time: " + str(start_time) + "\n")
 
