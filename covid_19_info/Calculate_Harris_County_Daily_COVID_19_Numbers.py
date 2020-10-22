8cc8de5cec8e351997a77495b0f246b305e8c7a9
Xuan Kuai <xuankuai91@gmail.com>
2020-10-22 09:55:10 -0500

    Use functions in custom module

--- covid_19_info/Calculate_Harris_County_Daily_COVID_19_Numbers.py
+++ covid_19_info/Calculate_Harris_County_Daily_COVID_19_Numbers.py
@@ -1,4 +1,6 @@
-import arcpy, datetime
+import arcpy, datetime, sys
+sys.path.append(r'\\Hgac.net\FileShare\ArcGIS\DataSvcs\GIS\Process Automation\Modules\Bin')
+import earcpy
 
 def calculate_harris_table(table, county_summary, day):
     fields = ["NAME", "No_of_Cases", "No_of_Actives", "No_of_Deaths", "No_of_Recoveries", "No_of_Tests"]
@@ -13,108 +15,82 @@ def calculate_harris_table(table, county_summary, day):
         tests = harris_row[5]
 
     # Compose date query of the execution date (for the latest cases data)
-    this_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(day.year, day.month, day.day)
-    this_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + this_day_date
+    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
 
     # Compose date query of the day before the execution date (for the previous cases data and the latest tests data)
     previous_day = day - datetime.timedelta(1)
-    previous_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_day.year, previous_day.month, previous_day.day)
-    previous_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_day_date
+    previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
 
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
 
     # Copy the latest cases data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Harris_County_Info
-    print("        Confirmed")
+    print("            Confirmed")
     arcpy.CalculateField_management('table_tv', "Confirmed", str(confirmed_cases))
 
-    print("        Active")
+    print("            Active")
     arcpy.CalculateField_management('table_tv', "Active", str(active_cases))
 
-    print("        Deceased")
+    print("            Deceased")
     arcpy.CalculateField_management('table_tv', "Deceased", str(deceased_cases))
 
-    print("        Recovered")
+    print("            Recovered")
     arcpy.CalculateField_management('table_tv', "Recovered", str(recovered_cases))
 
     # Calculate fatality rate
-    print("        Fatality_Rate")
+    print("            Fatality_Rate")
     arcpy.CalculateField_management('table_tv', "Fatality_Rate", "100 * [Deceased] / [Confirmed]")
 
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
 
     # Copy the latest tests data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Harris_County_Info
-    print("        Tests")
+    print("            Tests")
     arcpy.CalculateField_management('table_tv', "Tests", str(tests))
 
     # Calculate changes in cases
-    previous_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Confirmed", "Active", "Deceased", "Recovered", "Tests"], previous_day_query)
-    for previous_day_row in previous_day_cursor:
-        confirmed_change = confirmed_cases - previous_day_row[1] 
-        active_change = active_cases - previous_day_row[2]
-        deceased_change = deceased_cases - previous_day_row[3]
-        recovered_change = recovered_cases - previous_day_row[4]
+    confirmed_change = earcpy.calculate_difference(table, "Confirmed", this_day_query, previous_day_query)
+    active_change = earcpy.calculate_difference(table, "Active", this_day_query, previous_day_query)
+    deceased_change = earcpy.calculate_difference(table, "Deceased", this_day_query, previous_day_query)
+    recovered_change = earcpy.calculate_difference(table, "Recovered", this_day_query, previous_day_query)
 
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
 
-    print("        Confirmed_Change")
+    print("            Confirmed_Change")
     arcpy.CalculateField_management('table_tv', "Confirmed_Change", str(confirmed_change))
 
-    print("        Active_Change")
+    print("            Active_Change")
     arcpy.CalculateField_management('table_tv', "Active_Change", str(active_change))
 
-    print("        Deceased_Change")
+    print("            Deceased_Change")
     arcpy.CalculateField_management('table_tv', "Deceased_Change", str(deceased_change))
 
-    print("        Recovered_Change")
+    print("            Recovered_Change")
     arcpy.CalculateField_management('table_tv', "Recovered_Change", str(recovered_change))
 
     # Compose date query of two days before the execution date (for the previous tests data)
     previous_2_day = day - datetime.timedelta(2)
-    previous_2_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_2_day.year, previous_2_day.month, previous_2_day.day)
-    previous_2_day_query = arcpy.AddFieldDelimiters(table, "Date_") + " = " + previous_2_day_date
+    previous_2_day_query = earcpy.compose_single_date_query(table, "Date_", previous_2_day, "=")
 
     # Calculate total and daily positivity rates, and changes in tests
-    previous_2_day_cursor = arcpy.da.SearchCursor(table, ["Date_", "Tests"], previous_2_day_query)
-    for previous_2_day_row in previous_2_day_cursor:
-        tests_change = tests - previous_2_day_row[1]
+    tests_change = earcpy.calculate_difference(table, "Tests", previous_day_query, previous_2_day_query)
 
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
 
-    print("        Tests_Change")
+    print("            Tests_Change")
     arcpy.CalculateField_management('table_tv', "Tests_Change", str(tests_change))
 
-    print("        Positivity")
+    print("            Positivity")
     arcpy.CalculateField_management('table_tv', "Positivity", "100 * [Confirmed] / [Tests]")
 
-    print("        Positivity_Daily")
+    print("            Positivity_Daily")
     arcpy.CalculateField_management('table_tv', "Positivity_Daily", "100 * [Confirmed_Change] / [Tests_Change]")
 
     # Compose date query of the 14-day period from 13 days before the execution date to the execution date
-    past_2_weeks_day = day - datetime.timedelta(13)
-    past_2_weeks_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(past_2_weeks_day.year, past_2_weeks_day.month, past_2_weeks_day.day)
-    past_2_weeks_query = arcpy.AddFieldDelimiters(table, "Date_") + " BETWEEN " + past_2_weeks_day_date + " AND " + this_day_date
-
-    # Calculate 14-day average changes in cases
-    past_2_weeks_cursor = arcpy.da.SearchCursor(table, ["Date_", "Confirmed_Change", "Active_Change", "Deceased_Change", "Recovered_Change"], past_2_weeks_query)
-
-    confirmed_change_list = []
-    active_change_list = []
-    deceased_change_list = []
-    recovered_change_list = []
-
-    for past_2_weeks_row in past_2_weeks_cursor:
-        if past_2_weeks_row[1] is not None:
-            confirmed_change_list.append(past_2_weeks_row[1])
-
-        if past_2_weeks_row[2] is not None:
-            active_change_list.append(past_2_weeks_row[2])
-        
-        if past_2_weeks_row[3] is not None:
-            deceased_change_list.append(past_2_weeks_row[3])
-        
-        if past_2_weeks_row[4] is not None:
-            recovered_change_list.append(past_2_weeks_row[4])
+    past_2_weeks_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=14)
+    confirmed_change_list = earcpy.list_values(table, "Confirmed_Change", past_2_weeks_query)
+    active_change_list = earcpy.list_values(table, "Active_Change", past_2_weeks_query)
+    deceased_change_list = earcpy.list_values(table, "Deceased_Change", past_2_weeks_query)
+    recovered_change_list = earcpy.list_values(table, "Recovered_Change", past_2_weeks_query)
 
     confirmed_moving_average = float(sum(confirmed_change_list)) / len(confirmed_change_list)
     active_moving_average = float(sum(active_change_list)) / len(active_change_list)
@@ -123,35 +99,23 @@ def calculate_harris_table(table, county_summary, day):
 
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
 
-    print("        Confirmed_Mov_Avg")
+    print("            Confirmed_Mov_Avg")
     arcpy.CalculateField_management('table_tv', "Confirmed_Mov_Avg", str(confirmed_moving_average))
 
-    print("        Active_Mov_Avg")
+    print("            Active_Mov_Avg")
     arcpy.CalculateField_management('table_tv', "Active_Mov_Avg", str(active_moving_average))
 
-    print("        Deceased_Mov_Avg")
+    print("            Deceased_Mov_Avg")
     arcpy.CalculateField_management('table_tv', "Deceased_Mov_Avg", str(deceased_moving_average))
 
-    print("        Recovered_Mov_Avg")
+    print("            Recovered_Mov_Avg")
     arcpy.CalculateField_management('table_tv', "Recovered_Mov_Avg", str(recovered_moving_average))
 
-    print("        Positivity_Mov_Avg")
+    print("            Positivity_Mov_Avg")
     # Compose date query of the 14-day period from 2 weeks before the previous day of the execution date to two days before the execution date
-    previous_2_weeks_day = day - datetime.timedelta(14)
-    previous_2_weeks_day_date = "'{:02d}-{:02d}-{:02d} 00:00:00'".format(previous_2_weeks_day.year, previous_2_weeks_day.month, previous_2_weeks_day.day)
-    previous_2_weeks_query = arcpy.AddFieldDelimiters(table, "Date_") + " BETWEEN " + previous_2_weeks_day_date + " AND " + previous_day_date
-
-    # Calculate 14-day average positivity rate
-    previous_2_weeks_cursor = arcpy.da.SearchCursor(table, ["Date_", "Confirmed_Change", "Tests_Change"], previous_2_weeks_query)
-
-    confirmed_change_list_previous_day = []
-    tests_change_list = []
-    for previous_2_weeks_row in previous_2_weeks_cursor:
-        if previous_2_weeks_row[1] is not None:
-            confirmed_change_list_previous_day.append(previous_2_weeks_row[1])
-
-        if previous_2_weeks_row[2] is not None:
-            tests_change_list.append(previous_2_weeks_row[2])
+    previous_2_weeks_query = earcpy.compose_double_date_query(table, "Date_", end_day=previous_day, period=14)
+    confirmed_change_list_previous_day = earcpy.list_values(table, "Confirmed_Change", previous_2_weeks_query)
+    tests_change_list = earcpy.list_values(table, "Tests_Change", previous_2_weeks_query)
 
     positivity_moving_average = 100 * float(sum(confirmed_change_list_previous_day)) / sum(tests_change_list)
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
@@ -161,12 +125,13 @@ def main():
     arcpy.env.workspace = r"Database Connections\Global_SDE_(Global_Admin).sde"
     arcpy.env.overwriteOutput = True
 
+    print("    Calculating COVID-19 data of Harris County ...")
     today = datetime.datetime.today()
 
     county_summary = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Info\Global.GLOBAL_ADMIN.HGAC_Counties_COVID_19_Cases"
 
     # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Harris_County_Info, and calculate fatality rate, positivity rates, changes in cases, tests, and 7-day averages of changes in cases and positivity rate
-    print("    HGAC_COVID_19_Harris_County_Info")
+    print("        HGAC_COVID_19_Harris_County_Info")
     harris_info_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Harris_County_Info"
     calculate_harris_table(harris_info_table, county_summary, today)
 
@@ -174,8 +139,8 @@ if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
     print("Starting Calculate Harris County Daily COVID-19 Numbers tool")
-    print("Version 1.0")
-    print("Last update: 9/18/2020")
+    print("Version 1.2")
+    print("Last update: 10/20/2020")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
     print("Start time: " + str(start_time) + "\n")
 
