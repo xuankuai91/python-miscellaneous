3520edb9b9021ac7877946f7a8471e7fc855d9b3
Xuan Kuai <xuankuai91@gmail.com>
2021-02-09 13:52:15 -0600

    Update output text

--- covid_19_info/Calculate_Harris_County_Daily_COVID_19_Numbers.py
+++ covid_19_info/Calculate_Harris_County_Daily_COVID_19_Numbers.py
@@ -138,7 +138,7 @@ def main():
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
-    print("Starting Calculate Harris County Daily COVID-19 Numbers tool")
+    print("Starting Calculate Harris County Daily COVID-19 Numbers tool ...")
     print("Version 1.2")
     print("Last update: 10/20/2020")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
