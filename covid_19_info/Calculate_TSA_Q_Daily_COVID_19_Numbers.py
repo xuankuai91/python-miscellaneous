631c63bd5241e0ec62f1c02d5d21311cd8ceae47
Xuan Kuai <xuankuai91@gmail.com>
2021-02-09 13:51:05 -0600

    Add the function to automatically fetch data

--- covid_19_info/Calculate_TSA_Q_Daily_COVID_19_Numbers.py
+++ covid_19_info/Calculate_TSA_Q_Daily_COVID_19_Numbers.py
@@ -1,4 +1,4 @@
-import arcpy, datetime
+import arcpy, datetime, urllib, json
 
 def main():
     workspace = r"Database Connections\Global_SDE_(Global_Admin).sde"
@@ -6,8 +6,37 @@ def main():
     arcpy.env.overwriteOutput = True
 
     print("    HGAC_COVID_19_TSA_Q_Info")
+    day = datetime.datetime.today()
+    date = "{}/{}/{}".format(day.year, day.month, day.day)
+
+    # Fetch TSA Q hospitalization data from DSHS
+    url = "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/DSHS_COVID_Hospital_Data/FeatureServer/0/query?where=TSA%3D%27Q%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    total = data["features"][0]["attributes"]["TotalStaff"]
+    available = data["features"][0]["attributes"]["AvailHospi"]
+    covid_occupied = data["features"][0]["attributes"]["COVIDPatie"]
+
     tsa_q = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_TSA_Q_Info"
 
+    print("        Inserting new row ...")
+    try:
+        # Insert a new row to table
+        cursor = arcpy.InsertCursor(tsa_q)
+        row = cursor.newRow()
+
+        # Write date and fetched values
+        row.setValue("Date_", date)
+        row.setValue("Total", total)
+        row.setValue("Available", available)
+        row.setValue("COVID_Occupied", covid_occupied)
+        cursor.insertRow(row)
+
+    except:
+        pass
+
+    # Calculate derived values
     print("        Total_Occupied")
     arcpy.CalculateField_management(tsa_q, "Total_Occupied", "[Total] - [Available]")
 
@@ -23,7 +52,7 @@ def main():
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
-    print("Starting Calculate TSA Q Daily COVID-19 Numbers tool")
+    print("Starting Calculate TSA Q Daily COVID-19 Numbers tool ...")
     print("Version 1.2")
     print("Last update: 9/18/2020")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
