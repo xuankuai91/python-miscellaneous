e7a1f455001410b518b776438feea7363c4a19bc
Xuan Kuai <xuankuai91@gmail.com>
2022-02-16 16:46:25 -0600

    Remove the codes that updates active/recovered tables

--- covid_19_info/Revise_COVID_19_Numbers.py
+++ covid_19_info/Revise_COVID_19_Numbers.py
@@ -44,6 +44,14 @@ def revise_positivities(table, day):
     arcpy.CalculateField_management('table_tv', "Positivity", "100 * [Total] / [Tests]")
 
     print("            Positivity_Mov_Avg")
+    # Compose date query of the previous day before the day intended to revise
+    previous_day = day - datetime.timedelta(1)
+    previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
+
+    # Calculate change in test
+    change = earcpy.calculate_difference(table, "Tests", this_day_query, previous_day_query)
+    arcpy.CalculateField_management('table_tv', "Tests_Change", str(change))
+
     # Compose date query of the 7-day period from 1 week before the previous day of the execution date to two days before the execution date
     past_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=7)
 
@@ -52,74 +60,6 @@ def revise_positivities(table, day):
 
     positivity_moving_average = 100 * float(sum(confirmed_change_list)) / sum(tests_change_list)
     arcpy.CalculateField_management('table_tv', "Positivity_Mov_Avg", str(positivity_moving_average))
-
-def copy_counts(table, day, total_confirmed, total_deceased):
-    # Compose date query of the iterated date
-    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
-
-    arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
-
-    # Copy the total confirmed and deceased case numbers to HGAC_COVID_19_US_MSAs_Confirmed_and_Deceased_Cases
-    print("            Houston_Total_Confirmed")
-    arcpy.CalculateField_management('table_tv', "Houston_Total_Confirmed", str(total_confirmed))
-
-    print("            Houston_Total_Deceased")
-    arcpy.CalculateField_management('table_tv', "Houston_Total_Deceased", str(total_deceased))
-
-    # Compose date query of the day before the iterated date
-    previous_day = day - datetime.timedelta(1)
-    previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
-
-    # Calculate changes in cases
-    print("            Houston_Confirmed_Change")
-    confirmed_change = earcpy.calculate_difference(table, "Houston_Total_Confirmed", this_day_query, previous_day_query)
-    arcpy.CalculateField_management('table_tv', "Houston_Confirmed_Change", str(confirmed_change))
-
-    print("            Houston_Total_Deceased")
-    deceased_change = earcpy.calculate_difference(table, "Houston_Total_Deceased", this_day_query, previous_day_query)
-    arcpy.CalculateField_management('table_tv', "Houston_Deceased_Change", str(deceased_change))
-
-    # Compose date query of the 7-day period from 6 days before the iterated date to the iterated date
-    past_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=7)
-
-    # Calculate 7-day average changes in cases
-    print("            Houston_Confirmed_Mov_Avg")
-    confirmed_change_list = earcpy.list_values(table, "Houston_Confirmed_Change", past_week_query)
-    confirmed_moving_average = float(sum(confirmed_change_list)) / len(confirmed_change_list)
-    arcpy.CalculateField_management('table_tv', "Houston_Confirmed_Mov_Avg", str(confirmed_moving_average))
-
-    print("            Houston_Deceased_Mov_Avg")
-    deceased_change_list = earcpy.list_values(table, "Houston_Deceased_Change", past_week_query)
-    deceased_moving_average = float(sum(deceased_change_list)) / len(deceased_change_list)
-    arcpy.CalculateField_management('table_tv', "Houston_Deceased_Mov_Avg", str(deceased_moving_average))
-
-    # Calculate fatality rate
-    print("            Houston_Fatality_Rate")
-    arcpy.CalculateField_management('table_tv', "Houston_Fatality_Rate", "100 * [Houston_Total_Deceased] / [Houston_Total_Confirmed]")
-
-def revise_houston_msa_numbers(table, day):
-    # Fetch H-GAC counties' confirmed and deceased case numbers from SDE and add up for Houston MSA
-    total_confirmed = 0
-    total_deceased = 0
-
-    confirmed_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Confirmed_Cases_and_Tests"
-    deceased_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Deceased_Cases"
-
-    fields = ["Austin", "Brazoria", "Chambers", "Fort_Bend", "Galveston", "Harris", "Liberty", "Montgomery", "Waller"]
-    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
-
-    cursor = arcpy.da.SearchCursor(confirmed_table, fields, where_clause=this_day_query)
-    for row in cursor:
-        for i in range(0, 9):
-            total_confirmed += row[i]
-
-    cursor = arcpy.da.SearchCursor(deceased_table, fields, where_clause=this_day_query)
-    for row in cursor:
-        for i in range(0, 9):
-            total_deceased += row[i]
-
-    copy_counts(table, day, total_confirmed, total_deceased)
 
 def main():
     workspace = r"Database Connections\Global_SDE_(Global_Admin).sde"
@@ -143,25 +83,6 @@ def main():
         revise_change(confirmed_table, current_day)
         revise_positivities(confirmed_table, current_day)
 
-    # Reset the value of Positivity_Mov_Avg for the most current day to null
-    last_day_query = earcpy.compose_single_date_query(confirmed_table, "Date_", current_day, "=")
-
-    arcpy.MakeTableView_management(confirmed_table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", last_day_query)
-    arcpy.CalculateField_management('table_tv', "Positivity_Mov_Avg", "NULL")
-
-    # Calculate changes in active cases for the day intended to revise and the day before
-    print("    HGAC_COVID_19_Active_Cases")
-    active_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Active_Cases"
-    
-    future_days_query = earcpy.compose_single_date_query(active_table, "Date_", this_day, ">=")
-    future_days_cursor = arcpy.da.SearchCursor(active_table, ["Date_"], future_days_query, sql_clause=order_clause)
-    for future_days_row in future_days_cursor:
-        current_day = future_days_row[0]
-
-        print("        " + str(current_day))
-        revise_change(active_table, current_day)
-
     # Calculate changes in deceased cases for the day intended to revise and the day before
     print("    HGAC_COVID_19_Deceased_Cases")
     deceased_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Deceased_Cases"
@@ -174,36 +95,12 @@ def main():
         print("        " + str(current_day))
         revise_change(deceased_table, current_day)
 
-    # Calculate changes in recovered cases for the day intended to revise and the day before
-    print("    HGAC_COVID_19_Recovered_Cases")
-    recovered_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Recovered_Cases"
-    
-    future_days_query = earcpy.compose_single_date_query(recovered_table, "Date_", this_day, ">=")
-    future_days_cursor = arcpy.da.SearchCursor(recovered_table, ["Date_"], future_days_query, sql_clause=order_clause)
-    for future_days_row in future_days_cursor:
-        current_day = future_days_row[0]
-
-        print("        " + str(current_day))
-        revise_change(recovered_table, current_day)
-
-    # Copy changes to the national table
-    print("    HGAC_COVID_19_US_MSAs_Confirmed_and_Deceased_Cases")
-    msa_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_US_MSAs_Confirmed_and_Deceased_Cases"
-
-    future_days_query = earcpy.compose_single_date_query(msa_table, "Date_", this_day, ">=")
-    future_days_cursor = arcpy.da.SearchCursor(deceased_table, ["Date_"], future_days_query, sql_clause=order_clause)
-    for future_days_row in future_days_cursor:
-        current_day = future_days_row[0]
-
-        print("        " + str(current_day))
-        revise_houston_msa_numbers(msa_table, current_day)
-
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
     print("Starting Revise COVID-19 Numbers tool ...")
-    print("Version 1.2")
-    print("Last update: 2/24/2021")
+    print("Version 1.8")
+    print("Last update: 2/16/2022")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
     print("Start time: " + str(start_time) + "\n")
 
