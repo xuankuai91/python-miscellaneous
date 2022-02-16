ee3c159ed7c290f86ffbb6e43810bacf3c43f049
Xuan Kuai <xuankuai91@gmail.com>
2022-02-16 16:40:01 -0600

    Update to reflect changes in DSHS reporting

--- covid_19_info/Calculate_Daily_COVID_19_Numbers.py
+++ covid_19_info/Calculate_Daily_COVID_19_Numbers.py
@@ -15,13 +15,9 @@ def fetch_fbcph_case_counts(table, date):
 
     cases = data["features"][0]["attributes"]["confirmed"]
     fatalities = data["features"][0]["attributes"]["deaths"]
-    recoveries = data["features"][0]["attributes"]["recovered"]
-    actives = cases - fatalities - recoveries
 
     arcpy.CalculateField_management('table_tv', "No_of_Cases", str(cases))
-    arcpy.CalculateField_management('table_tv', "No_of_Actives", str(actives))
     arcpy.CalculateField_management('table_tv', "No_of_Deaths", str(fatalities))
-    arcpy.CalculateField_management('table_tv', "No_of_Recoveries", str(recoveries))
 
     arcpy.CalculateField_management('table_tv', "Last_Updated", date)
 
@@ -92,6 +88,24 @@ def calculate_change(table, day):
 
     arcpy.CalculateField_management('table_tv', "Mov_Avg", str(moving_average))
 
+def fetch_test_counts(table, county):
+    print("            " + county)
+    query = "{} = '{}'".format(arcpy.AddFieldDelimiters(table, "NAME"), county)
+    arcpy.MakeTableView_management(table, 'table_tv')
+    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
+
+    # Convert "Fort Bend" to "Fort+Bend" to fit URL format
+    if county == "Fort Bend":
+        county = "Fort+Bend"
+
+    # Fetch test data from DSHS
+    url = "https://services3.arcgis.com/vljlarU2635mITsl/ArcGIS/rest/services/covid19_test_data_hosted/FeatureServer/0/query?where=county%3D%27{}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=".format(county)
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    tests = data["features"][0]["attributes"]["total_tests"]
+    arcpy.CalculateField_management('table_tv', "No_of_Tests", str(tests))
+
 def calculate_test_count(table, county_summary, day):
     print("        Tests")
     county_cursor = arcpy.da.SearchCursor(county_summary, ["No_of_Tests"])
@@ -100,42 +114,39 @@ def calculate_test_count(table, county_summary, day):
     total_tests = sum(test_list)
 
     # Compose date query of two days before the execution date (for the previous cases data)
-    previous_day = day - datetime.timedelta(1)
-    previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
+    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
 
     arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
+    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
 
     arcpy.CalculateField_management('table_tv', "Tests", str(total_tests))
 
 def calculate_positivities(table, county_summary, day):
     print("        Positivity")
+    this_day_query = earcpy.compose_single_date_query(table, "Date_", day, "=")
+
+    arcpy.MakeTableView_management(table, 'table_tv')
+    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
+    arcpy.CalculateField_management('table_tv', "Positivity", "100 * [Total] / [Tests]")
+
+    print("        Positivity_Mov_Avg")
     # Compose date query of the day before the execution date (for the latest cases data)
     previous_day = day - datetime.timedelta(1)
     previous_day_query = earcpy.compose_single_date_query(table, "Date_", previous_day, "=")
 
-    arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
-    arcpy.CalculateField_management('table_tv', "Positivity", "100 * [Total] / [Tests]")
-
-    print("        Positivity_Mov_Avg")
-    # Compose date query of two days before the execution date (for the previous tests data)
-    previous_2_day = day - datetime.timedelta(2)
-    previous_2_day_query = earcpy.compose_single_date_query(table, "Date_", previous_2_day, "=")
-
-    tests_change = earcpy.calculate_difference(table, "Tests", previous_day_query, previous_2_day_query)
+    tests_change = earcpy.calculate_difference(table, "Tests", this_day_query, previous_day_query)
 
     arcpy.CalculateField_management('table_tv', "Tests_Change", str(tests_change))
 
     # Compose date query of the 7-day period from 1 week before the previous day of the execution date to two days before the execution date
-    previous_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=previous_day, period=7)
+    past_week_query = earcpy.compose_double_date_query(table, "Date_", end_day=day, period=7)
 
     # Calculate 7-day average positivity rate
-    confirmed_change_list = earcpy.list_values(table, "Change", previous_week_query)
-    tests_change_list = earcpy.list_values(table, "Tests_Change", previous_week_query)
+    confirmed_change_list = earcpy.list_values(table, "Change", past_week_query)
+    tests_change_list = earcpy.list_values(table, "Tests_Change", past_week_query)
 
     positivity_moving_average = 100 * float(sum(confirmed_change_list)) / sum(tests_change_list)
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", previous_day_query)
+    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", this_day_query)
     arcpy.CalculateField_management('table_tv', "Positivity_Mov_Avg", str(positivity_moving_average))
 
 def calculate_table(table, fields, county_summary, day):
@@ -152,12 +163,16 @@ def main():
     print("    HGAC_Counties_COVID_19_Cases")
     county_summary = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Info\Global.GLOBAL_ADMIN.HGAC_Counties_COVID_19_Cases"
 
-    if yesterday.strftime('%w') in ("0", "1", "2", "3", "4"): # Update Chambers County and Montgomery County cases on weekdays only
-        print("        No_of_Cases, No_of_Actives, No_of_Deaths, No_of_Recoveries")
-        fetch_fbcph_case_counts(county_summary, "{}/{}/{}".format(yesterday.year, yesterday.month, yesterday.day))
+    if yesterday.strftime('%w') in ("0", "1", "2", "3", "4"): # Update Fort Bend County cases on weekdays only
+        print("        No_of_Cases, No_of_Deaths")
+        yesterday_date = "{}/{}/{}".format(yesterday.year, yesterday.month, yesterday.day)
+        fetch_fbcph_case_counts(county_summary, "CDate(#{}#)".format(yesterday_date))
 
-    print("        No_of_Cases")
-    arcpy.CalculateField_management(county_summary, "No_of_Cases", "[No_of_Actives] + [No_of_Deaths] + [No_of_Recoveries]") # Calculate total number of cases
+    # Fetch test counts for the previous day
+    print("        No_of_Tests")
+    counties = ["Austin", "Brazoria", "Chambers", "Colorado", "Fort Bend", "Galveston", "Harris", "Liberty", "Matagorda", "Montgomery", "Walker", "Waller", "Wharton"]
+    for county in counties:
+        fetch_test_counts(county_summary, county)
 
     print("        Positivity")
     arcpy.CalculateField_management(county_summary, "Positivity", "100 * [No_of_Cases] / [No_of_Tests]") # Calculate total positivity
@@ -169,27 +184,17 @@ def main():
     calculate_test_count(confirmed_table, county_summary, yesterday)
     calculate_positivities(confirmed_table, county_summary, yesterday)
 
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Active_Cases, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Active_Cases")
-    active_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Active_Cases"
-    calculate_table(active_table, ["NAME", "No_of_Actives"], county_summary, yesterday)
-
     # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Deceased_Cases, and calculate total, change, and 7-day average
     print("    HGAC_COVID_19_Deceased_Cases")
     deceased_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Deceased_Cases"
     calculate_table(deceased_table, ["NAME", "No_of_Deaths"], county_summary, yesterday)
 
-    # Copy data from HGAC_Counties_COVID_19_Cases to HGAC_COVID_19_Recovered_Cases, and calculate total, change, and 7-day average
-    print("    HGAC_COVID_19_Recovered_Cases")
-    recovered_table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Recovered_Cases"
-    calculate_table(recovered_table, ["NAME", "No_of_Recoveries"], county_summary, yesterday)
-
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
     print("Starting Calculate Daily COVID-19 Numbers tool ...")
-    print("Version 1.4")
-    print("Last update: 10/20/2020")
+    print("Version 1.9")
+    print("Last update: 2/16/2022")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
     print("Start time: " + str(start_time) + "\n")
 
