ea068ac98d8e8b41ddd35712b2849f4a4bd8d138
Xuan Kuai <xuankuai91@gmail.com>
2022-02-16 16:45:08 -0600

    Remove the test fetching feature

--- covid_19_info/Fetch_Daily_COVID_19_Numbers.py
+++ covid_19_info/Fetch_Daily_COVID_19_Numbers.py
@@ -1,37 +1,27 @@
 import arcpy, datetime, urllib, json
 
-def fetch_dshs_case_counts(table, county, date):
+def fetch_dshs_case_counts(table, county, day):
+    # Offset date discrepancy from DSHS
+    pseudo_day = day + datetime.timedelta(1)
+    pseudo_date = "{}/{}/{}".format(pseudo_day.year, pseudo_day.month, pseudo_day.day)
+
     print("            " + county)
     query = "{} = '{}'".format(arcpy.AddFieldDelimiters(table, "NAME"), county)
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
 
     # Fetch case data from DSHS
-    url = "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/TX_DSHS_COVID19_Cases_Service/FeatureServer/1/query?where=County%3D%27{}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=".format(county)
+    url = "https://services3.arcgis.com/vljlarU2635mITsl/ArcGIS/rest/services/covid19_case_data_hosted/FeatureServer/0/query?where=county%3D%27{}%27+AND+year%3DTIMESTAMP+%27{}+6%3A00%3A00+AM%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=".format(county, pseudo_date.replace("/", "%2F"))
     response = urllib.urlopen(url)
     data = json.loads(response.read())
 
-    actives = data["features"][0]["attributes"]["Active"]
-    fatalities = data["features"][0]["attributes"]["Fatalities"]
-    recoveries = data["features"][0]["attributes"]["Recoveries"]
-    cases = actives + fatalities + recoveries
-
-    if county == "Waller": # Update number of deaths only for Waller County
-        cases = data["features"][0]["attributes"]["Positive"]
-        actives = data["features"][0]["attributes"]["Active"]
-        fatalities = data["features"][0]["attributes"]["Fatalities"]
-        recoveries = cases - actives - fatalities
-    else:
-        actives = data["features"][0]["attributes"]["Active"]
-        fatalities = data["features"][0]["attributes"]["Fatalities"]
-        recoveries = data["features"][0]["attributes"]["Recoveries"]
-        cases = actives + fatalities + recoveries
+    cases = data["features"][0]["attributes"]["confirmed"]
+    fatalities = data["features"][0]["attributes"]["fatalities"]
     
     arcpy.CalculateField_management('table_tv', "No_of_Cases", str(cases))
-    arcpy.CalculateField_management('table_tv', "No_of_Actives", str(actives))
     arcpy.CalculateField_management('table_tv', "No_of_Deaths", str(fatalities))
-    arcpy.CalculateField_management('table_tv', "No_of_Recoveries", str(recoveries))
 
+    date = "{}/{}/{}".format(day.year, day.month, day.day)
     arcpy.CalculateField_management('table_tv', "Last_Updated", "CDate(#{}#)".format(date))
 
 def fetch_hcph_case_counts(table, day, date):
@@ -40,7 +30,7 @@ def fetch_hcph_case_counts(table, day, date):
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
 
-    # Fetch case data from DSHS
+    # Fetch case data from Harris County
     url = "https://services.arcgis.com/su8ic9KbA7PYVxPS/ArcGIS/rest/services/Download_Current_COVID_Case_Counts/FeatureServer/0/query?where=Source%3D%27All%27&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
     response = urllib.urlopen(url)
     data = json.loads(response.read())
@@ -51,9 +41,7 @@ def fetch_hcph_case_counts(table, day, date):
     cases = actives + fatalities + recoveries
 
     arcpy.CalculateField_management('table_tv', "No_of_Cases", str(cases))
-    arcpy.CalculateField_management('table_tv', "No_of_Actives", str(actives))
     arcpy.CalculateField_management('table_tv', "No_of_Deaths", str(fatalities))
-    arcpy.CalculateField_management('table_tv', "No_of_Recoveries", str(recoveries))
 
     arcpy.CalculateField_management('table_tv', "Last_Updated", "CDate(#{}#)".format(date))
 
@@ -63,20 +51,16 @@ def fetch_ccph_case_counts(table, date):
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
 
-    # Fetch case data from DSHS
+    # Fetch case data from Chambers County
     url = "https://services8.arcgis.com/5rne9bHBCbjGEtW8/ArcGIS/rest/services/Chambers_Case_Count/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
     response = urllib.urlopen(url)
     data = json.loads(response.read())
 
     cases = data["features"][0]["attributes"]["ConfirmedCases"]
     fatalities = data["features"][0]["attributes"]["TotalDeaths"]
-    recoveries = data["features"][0]["attributes"]["TotalRecovered"]
-    actives = cases - fatalities - recoveries
 
     arcpy.CalculateField_management('table_tv', "No_of_Cases", str(cases))
-    arcpy.CalculateField_management('table_tv', "No_of_Actives", str(actives))
     arcpy.CalculateField_management('table_tv', "No_of_Deaths", str(fatalities))
-    arcpy.CalculateField_management('table_tv', "No_of_Recoveries", str(recoveries))
 
 def fetch_mcph_case_counts(table, date):
     print("            Montgomery")
@@ -84,40 +68,18 @@ def fetch_mcph_case_counts(table, date):
     arcpy.MakeTableView_management(table, 'table_tv')
     arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
 
-    # Fetch case data from DSHS
+    # Fetch case data from Montgomery County
     url = "https://services1.arcgis.com/PRoAPGnMSUqvTrzq/ArcGIS/rest/services/CoronavirusCases_current/FeatureServer/0/query?where=Name%3D%27Montgomery+County%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
     response = urllib.urlopen(url)
     data = json.loads(response.read())
 
     cases = data["features"][0]["attributes"]["positive"]
-    actives = data["features"][0]["attributes"]["active"]
     fatalities = data["features"][0]["attributes"]["deaths"]
-    recoveries = cases - actives - fatalities
 
     arcpy.CalculateField_management('table_tv', "No_of_Cases", str(cases))
-    arcpy.CalculateField_management('table_tv', "No_of_Actives", str(actives))
     arcpy.CalculateField_management('table_tv', "No_of_Deaths", str(fatalities))
-    arcpy.CalculateField_management('table_tv', "No_of_Recoveries", str(recoveries))
 
     arcpy.CalculateField_management('table_tv', "Last_Updated", "CDate(#{}#)".format(date))
-
-def fetch_test_counts(table, county):
-    print("            " + county)
-    query = "{} = '{}'".format(arcpy.AddFieldDelimiters(table, "NAME"), county)
-    arcpy.MakeTableView_management(table, 'table_tv')
-    arcpy.SelectLayerByAttribute_management('table_tv', "NEW_SELECTION", query)
-
-    # Convert "Fort Bend" to "Fort+Bend" to fit URL format
-    if county == "Fort Bend":
-        county = "Fort+Bend"
-
-    # Fetch test data from DSHS
-    url = "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/TX_DSHS_COVID19_TestData_Service/FeatureServer/0/query?where=County%3D%27{}%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=".format(county)
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-
-    tests = data["features"][0]["attributes"]["Cumulative"]
-    arcpy.CalculateField_management('table_tv', "No_of_Tests", str(tests))
 
 def main():
     workspace = r"Database Connections\Global_SDE_(Global_Admin).sde"
@@ -130,9 +92,9 @@ def main():
     table = r"Global.GLOBAL_ADMIN.HGAC_COVID_19_Info\Global.GLOBAL_ADMIN.HGAC_Counties_COVID_19_Cases"
 
     print("    HGAC_Counties_COVID_19_Cases")
-    print("        No_of_Cases, No_of_Actives, No_of_Deaths, No_of_Recoveries")
+    print("        No_of_Cases, No_of_Deaths")
     for county in ["Austin", "Colorado", "Liberty", "Matagorda", "Walker", "Waller", "Wharton"]:
-        fetch_dshs_case_counts(table, county, date)
+        fetch_dshs_case_counts(table, county, day)
 
     fetch_hcph_case_counts(table, day, date)
 
@@ -142,18 +104,12 @@ def main():
     if day.strftime('%w') == "3": # Update Montgomery County cases on Wednesdays only
         fetch_mcph_case_counts(table, date)
 
-    print("        No_of_Tests")
-    counties = ["Austin", "Brazoria", "Chambers", "Colorado", "Fort Bend", "Galveston", "Harris", "Liberty", "Matagorda", "Montgomery", "Walker", "Waller", "Wharton"]
-
-    for county in counties:
-        fetch_test_counts(table, county)
-
 if __name__ == "__main__":
     start_time = datetime.datetime.now()
 
     print("Starting Fetch Daily COVID-19 Numbers tool ...")
-    print("Version 1.0")
-    print("Last update: 10/20/2020")
+    print("Version 1.4")
+    print("Last update: 2/16/2022")
     print("Support: Xuan.Kuai@h-gac.com" + "\n")
     print("Start time: " + str(start_time) + "\n")
 
