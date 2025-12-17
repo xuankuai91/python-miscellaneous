59dd975ec760b06984ad64d240916317bd8d7a9a
Xuan Kuai <xuankuai91@gmail.com>
2025-12-17 10:56:09 -0600

    Create email_config.py

---
+++ webgisdr_health/email_config.py
@@ -0,0 +1,10 @@
+SMTP_USERNAME = "AKIAYNBGWEMPXGVAQB7R"
+SMTP_PASSWORD = "BEIjXUukFXHVi2dnXxIE6x0U+JVsD3IjlLhIiGTwA341"
+SMTP_SERVER = "email-smtp.us-east-1.amazonaws.com"
+SMTP_PORT = 587
+
+SENDER_EMAIL = "RiVA-Houston-Support@woodside.com"
+RECEIVERS_LIST = [
+    "travis.jowell@woodside.com",
+    "XUAN.KUAI@woodside.com"
+]
